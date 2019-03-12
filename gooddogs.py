4a98c9056013c027f734f07669b345142ad85465
Maygan <mnlightstone@gmail.com>
2019-03-11 20:13:49 -0700

    better error checking, added function to wait 15 minutes if we hit a limit-exceed error

--- gooddogs.py
+++ gooddogs.py
@@ -1,34 +1,54 @@
 import tweepy
 from credentials import *
 from time import sleep
-
+import sys
 
 def setup():
+    api = None
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    return api
+
+    while api is None:
+        try:
+            api = tweepy.API(auth)
+            return api
+
+        except tweepy.TweepError as e:
+            print(e)
+            if e.api_code == 429:
+                wait_fifteen_minutes(e)
 
 
 def run_search(cursor):
+    blacklist = ['designs', 'company', 'mr dad official', 'co', 'store', 'trainer']
+
+    # counter to 100 so that we periodically get a new batch of tweets instead of running through ALL of the
+    # historic tweets
+    count = 0
+    GET_NEW_TWEETS = 100
+
     for tweet in cursor:
+        if count > GET_NEW_TWEETS:
+            break
         try:
 
-            # if this post has a picture or video AND it is not a retweet
-            if 'media' in tweet.entities and 'RT ' not in tweet.full_text:
+            # if this post has a picture or video AND it is not a retweet AND it is not in our blacklist list
+            if 'media' in tweet.entities \
+                    and 'RT ' not in tweet.full_text\
+                    and not any(s in tweet.author.screen_name.lower() for s in blacklist):
 
                 # retweet
-                tweet.retweet()
-                print('Retweeted a tweet')
+                # tweet.retweet()
+                # print('Retweeted a tweet')
                 print(tweet.entities['media'][0]['media_url'], "\n")
 
-                # wait 2:00
-                sleep(120)
-            # else:
-            #     print("skipping id" + tweet.id)
+                # wait 5:00
+                sleep(0)
+                count += 1
 
         except tweepy.TweepError as e:
-            print(e.reason)
+            if e.api_code == 429:
+                wait_fifteen_minutes(e)
 
         except KeyError:
             print("No media to retweet")
@@ -36,27 +56,33 @@ def run_search(cursor):
         except StopIteration:
             break
 
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            print("something else went wrong")
+
+
+def wait_fifteen_minutes(error):
+    print("waiting 15 min")
+    print(error.reason)
+    sleep(300)
+
 
 if __name__ == '__main__':
-    first = True
     api = setup()
-
-
+    # filte
+    # gooddog filter:media -filter:retweets
     # run forever
     while True:
+        try:
+            # make cursor with hashtags, including 280char tweets
+            tweepy_cursor = tweepy.Cursor(api.search, q='#gooddog OR #cutedog OR #dogsoftwitter', tweet_mode='extended').items()
 
-        # if running for the first time, start at the beginning
-        if first:
-            starting_id = 0
+            # perform search and retweets
+            run_search(tweepy_cursor)
 
-        # otherwise, start where I last tweeted
-        else:
-            starting_id = api.user_timeline("gooddogs10", count=1)
-            starting_id = starting_id[0].id
-            first = False
+        # if getting the cursor fails, it most likely means we have hit the 15 requests in 15 minutes error
+        except tweepy.TweepError as e:
+            if e.api_code == 429:
+                wait_fifteen_minutes(e)
 
-        # make cursor with hashtags, starting ID, and getting 280char tweets
-        tweepy_cursor = tweepy.Cursor(api.search, q='#gooddog OR #cutedog OR #dogsoftwitter', since_id=starting_id, tweet_mode='extended').items()
 
-        # perform search and retweets
-        run_search(tweepy_cursor)
